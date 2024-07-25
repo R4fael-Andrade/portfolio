b5f9c43aa68d0198b5130a9e9c27162c6c69809f
Rafael <rafael.sousadrd@gmail.com>
2024-07-25 14:56:21 -0300

    construindo a estrutura e a seção "sobre mim"

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,17 @@ const Sobre = () => (
       distinctio commodi iste, soluta facilis dolores, quidem natus recusandae
       dicta eveniet excepturi?
     </Paragrafo>
+
+    <GithubSecao>
+      <img
+        src="https://github-readme-stats.vercel.app/api?username=R4fael-Andrade&hide_title=false&hide_rank=false&show_icons=true&include_all_commits=true&count_private=false&disable_animations=false&theme=vue-dark&locale=en&hide_border=false&order=1"
+        alt="stats graph"
+      />
+      <img
+        src="https://github-readme-stats.vercel.app/api/top-langs?username=R4fael-Andrade&locale=en&hide_title=false&layout=compact&card_width=320&langs_count=5&theme=vue-dark&hide_border=false&order=2"
+        alt="languages graph"
+      />
+    </GithubSecao>
   </section>
 )
 
